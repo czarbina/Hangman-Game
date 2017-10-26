0b2afe2ee0b06458722be291e4f01487d7fdac4a
Sabrina Kesler <sabrina@getvictorious.com>
2017-10-25 18:57:37 -0700

    For every onkeyup, we have a function that will capture the letter, and convert it to lowercase if need be.

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Create object with word properties per word and answers as strings in an array
+	var words = {
+		w1: ["c","u","t","t","l","e","f","i","s","h"],
+		w2: ["n","a","r","w","h","a","l"],
+		w3: ["m","a","n","a","t","e","e"],
+		w4: ["p","l","a","t","y","p","u","s"]
+	};
+
+	var wins = 0;
+	var guessesLeft = 15;
+	var alreadyGuessed = [];
